d8c9027d065505734f6f757b253d236f1e93ff74
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-08-06 01:38:35 +0900

    회원가입 페이지 연동

--- myshop/account/models.py
+++ myshop/account/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-class Account(models.Moel):
+class Account(models.Model):
     email = models.EmailField(max_length=50, unique=True, null=True, blank=True)
     password = models.CharField(max_length=50)
     name = models.CharField(max_length=50, null=True, blank=True)
-    phone_number = models.CharField(max_length=11, unique=True, validators=[
-        RegexValidator('^09\d{9}$', message="Invalid phone number.")])
-    date_joined = model.DateTimeField(auto_now_add=True)
-    
+    phone_number = models.CharField(max_length=11, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         db_table = "accounts"
