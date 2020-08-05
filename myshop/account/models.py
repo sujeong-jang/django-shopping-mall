abd70048267382054d8e8d1df5bbccd16decd7a0
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-08-06 00:30:29 +0900

    account model 생성

--- myshop/account/models.py
+++ myshop/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Moel):
+    email = models.EmailField(max_length=50, unique=True, null=True, blank=True)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    phone_number = models.CharField(max_length=11, unique=True, validators=[
+        RegexValidator('^09\d{9}$', message="Invalid phone number.")])
+    date_joined = model.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = "accounts"
