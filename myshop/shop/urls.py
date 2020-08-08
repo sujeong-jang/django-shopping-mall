e2e74e206b3aab7aaa76bece0be1819ae3c25faa
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-08-09 01:29:22 +0900

    상품 조회, 장바구니, 결제 화면 구현

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -3,4 +3,10 @@ from shop import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("outer/", views.outer, name="outer"),
+    path("top/", views.top, name="top"),
+    path("skirt/", views.skirt, name="skirt"),
+    path("pants/", views.pants, name="pants"),
+    path("cart/", views.cart, name="cart"),
+    path("cart/checkout/", views.checkout, name="checkout"),
 ]
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 
 def home(request):
    return render(request, 'shop/index.html')
+
+def outer(request):
+   return render(request, 'shop/outer.html')
+
+def top(request):
+   return render(request, 'shop/top.html')
+
+def skirt(request):
+   return render(request, 'shop/skirt.html')
+
+def pants(request):
+   return render(request, 'shop/pants.html')
+
+def cart(request):
+   return render(request, 'shop/cart.html')   
+
+def checkout(request):
+   return render(request, 'shop/checkout.html')
